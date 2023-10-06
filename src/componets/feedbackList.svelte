231d603279c9a370678aeccfdd42b6ac1586a3ac
Zayn Yusuf <yusue786@gmail.com>
2023-10-06 20:02:50 +0600

    first commit

---
+++ src/componets/feedbackList.svelte
@@ -0,0 +1,11 @@
+<script>
+    import FeedbackItems from "./feedbackItems.svelte";
+    import {FeedbackStore} from '../store.js'
+  import { fade, scale } from 'svelte/transition'
+</script>
+
+{#each $FeedbackStore as feedback (feedback.id) }
+ <div in:scale out:fade="{{ duration: 500 }}">
+  <FeedbackItems item={feedback}  />
+ </div>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import FeedbackFrom from "../componets/feedbackFrom.svelte";
+    import FeedbackStats from "../componets/feedbackStats.svelte";
+    import FeedbackList from "../componets/feedbackList.svelte";
+   
+</script>
+
+<main class="container">
+    <FeedbackFrom/>
+    <FeedbackStats />
+    <FeedbackList />
+</main>
+
